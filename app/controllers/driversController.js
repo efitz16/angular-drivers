6e8453aa73759030de3a7f5a6799772fc32dbc33
Elizabeth Fitzpatrick <elizabethfitzpatrick16@gmail.com>
2017-04-04 19:40:00 -0400

    get change score button to work for single driver template

--- app/controllers/driversController.js
+++ app/controllers/driversController.js
@@ -1,10 +1,9 @@
 app.controller('driversController', ['$scope', 'getDrivers', function($scope, getDrivers) {
   getDrivers.success(function(data) { 
   	$scope.driversList = data.MRData.StandingsTable.StandingsLists[0].DriverStandings;
-  	$scope.increasePoints = function(index) {
-  		// debugger;
-  	  var newScore = parseInt($scope.driversList[index].points) + 1;
-  	  $scope.driversList[index].points = newScore;
-  	}
   });
+  // $scope.increasePoints = function(index) {
+  // 	  var newScore = parseInt($scope.driversList[index].points) + 1;
+  // 	  $scope.driversList[index].points = newScore;
+  // 	}
 }]);
--- app/controllers/singleDriverController.js
+++ app/controllers/singleDriverController.js
@@ -2,5 +2,9 @@ app.controller('singleDriverController', ['$scope', 'getDrivers', '$routeParams'
 	getDrivers.success(function(data) {
 	  $scope.detail = data.MRData.StandingsTable.StandingsLists[0].DriverStandings[$routeParams.id];
 	  $scope.index = $routeParams.id;
+	  $scope.increasePoints = function() {
+  	  var newScore = parseInt($scope.detail.points) + 1;
+  	  $scope.detail.points = newScore;
+  	}
 	});
 }]);
